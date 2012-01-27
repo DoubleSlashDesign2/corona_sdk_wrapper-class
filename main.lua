aa23340e59772decc5c8ffa38e563fd04ea13a74
Michael Milwa <mike@milwa.de>
2012-01-27 21:48:21 +0100

    little change

--- main.lua
+++ main.lua
@@ -9,31 +9,19 @@ local _W = display.contentWidth
 local _H = display.contentHeight
 
 myParagraph = Wrapper:newParagraph({
-<<<<<<< HEAD
+
 	text = "Corona's framework dramatically increase productivity. Tasks like animating objects in OpenGL or creating user-interface widgets take only one line of code, and changes are instantly viewable in the Corona Simulator.",
-	--text = "Corona'sframeworkdramaticallyincreaseproductivity.TaskslikeanimatingobjectsinOpenGLorcreatinguserinterfacewidgetstakeonlyonelineofcode,andhangesareinstantlyviewableintheCoronaSimulator. ",
 	width = 200,
 	--height = 200, 			-- fontSize will be calculated automatically if set 
 	--font = "helvetica", 	-- make sure the selected font is installed on your system
 	fontSize = 14,
 	lineSpace = 5,
 	alignment  = "left"
-=======
-	text = "Corona's framework dramatically increase productivity. Tasks like animating objects in OpenGL or creating user-interface widgets take only one line of code, and changes are instantly viewable in the Corona Simulator. ",
-	width = 220,
-	--height = 80, 			-- fontSize will be calculated automatically if set 
-	--font = "helvetica", 	-- make sure the selected font is installed on your system
-	fontSize = 14,
-	lineSpace = 5,
-	alignment  = "center"
->>>>>>> ed8fca8278326b6d951d88618daef9d2dbac5b4b
 })
 myParagraph:setReferencePoint(display.TopCenterReferencePoint)
 myParagraph.x = _W/2
 myParagraph.y = 50
 myParagraph:setTextColor({255,255})
-
---print(myParagraph.width)
 
 rect = display.newRoundedRect(0,0,myParagraph.width+15, myParagraph.height+15, 15 )
 rect:setReferencePoint(display.CenterReferencePoint)
